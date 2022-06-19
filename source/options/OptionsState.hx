394d0547fe88e7ffbcb5cad0c600dd832d002c52
Zhamp <91746799+TheZhamp@users.noreply.github.com>
2022-06-19 09:47:45 -0300

    Add files via upload

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -59,6 +59,8 @@ class OptionsState extends MusicBeatState
 		DiscordClient.changePresence("Options Menu", null);
 		#end
 
+		FlxG.mouse.visible = false;
+
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.color = 0xFFea71fd;
 		bg.updateHitbox();
